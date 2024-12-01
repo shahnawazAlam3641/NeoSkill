7a068a5dc48c07061398731e3cef5cf34f3b9c00
shahnawazAlam3641 <shahnawazalam3641@gmail.com>
2024-12-01 15:02:09 +0530

    add react app url to fix cors issue

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.REACT_APP_BASE_URL,
     credentials: true,
   })
 );
